4b4ebf65470f5e8afa043549d82faccd6edb4ff2
Stas Nedosekin <nedosekinstanislav@gmail.com>
2020-11-16 23:50:21 +0300

    db connect create

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const db = require('./db');
 
 const PORT = process.env.PORT || 3000;
 
 // Object app
 const app = express();
 
-app.listen(PORT, () => {
-    console.log('server started')
-})
+async function start() {
+  try {
+    // await mongoose.connect(url, {
+    //   useNewUrlParser: true, 
+    //   useUnifiedTopology: true
+    // })
+    app.listen(PORT, () => {
+        console.log('server started')
+    })
+  } catch (error) {
+      console.log(error)
+  }
+}
+
+start()
