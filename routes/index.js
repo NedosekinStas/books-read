29e23a8f4ae5d8d2761dfcfce1d42397841499f7
Stas Nedosekin <nedosekinstanislav@gmail.com>
2020-11-25 13:57:45 +0300

    add get router object, condition active link class

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,18 @@
-const {Router} = require('express');
+const { Router } = require('express');
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.render('index');
-})
+  res.render('index', {
+      title: 'Books List',
+      isIndex: true
+  });
+});
 
-module.exports = router
+router.get('/create', (req,res) => {
+  res.render('create', {
+      title: 'Create Book',
+      isCreate: true
+  });
+});
+
+module.exports = router;
