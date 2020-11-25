f328e4ef48ccae0f2d56ad3c893979cb9cf2d7d5
Stas Nedosekin <nedosekinstanislav@gmail.com>
2020-11-25 05:20:27 +0300

    add handlebars, true connect with mongodb Compass, create chunks head and navbar, create lauout

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const db = require('./db');
+const exphbs = require('express-handlebars');
+const in_routes = require('./routes/index');
 
 const PORT = process.env.PORT || 3000;
 
 // Object app
 const app = express();
 
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs'
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+app.use(in_routes)
+
+
 async function start() {
   try {
-    // await mongoose.connect(url, {
-    //   useNewUrlParser: true, 
-    //   useUnifiedTopology: true
-    // })
+    await mongoose.connect('mongodb://localhost:27017/books_read', {
+      useNewUrlParser: true, 
+      useUnifiedTopology: true
+    })
     app.listen(PORT, () => {
         console.log('server started')
     })
